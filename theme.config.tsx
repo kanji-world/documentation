8e58a50eb12abfeb7267c2c6f6f35378be30ef56
Vladyslav Dalechyn <vlad@kanji.io>
2023-03-23 16:53:21 +0200

    feat: nextra docs with hooks documentation

---
+++ theme.config.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <bold>Kanji SDK</bold>,
+  project: {
+    link: 'https://github.com/kanji-world/documentation',
+  },
+  docsRepositoryBase: 'https://github.com/kanji-world/documentation',
+  footer: {
+    text: 'Kanji SDK',
+  },
+}
+
+export default config
